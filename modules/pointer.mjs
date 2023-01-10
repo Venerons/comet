922cdd4ce5f81bb73fa4554fbd8fedbc0064d781
Daniele Veneroni <Venerons@users.noreply.github.com>
2023-01-10 15:22:15 +0100

    Update pointer.mjs

--- modules/pointer.mjs
+++ modules/pointer.mjs
@@ -1,10 +1,14 @@
 class CometPointerController {
+	#surface;
+	#on_start;
+	#on_update;
+	#on_stop;
 	constructor(options) {
 		const t = this;
-		t.surface = options.surface;
-		t.on_start = options.on_start;
-		t.on_update = options.on_update;
-		t.on_stop = options.on_stop;
+		t.#surface = options.surface;
+		t.#on_start = options.on_start;
+		t.#on_update = options.on_update;
+		t.#on_stop = options.on_stop;
 		const pointers = new Set();
 		t.surface.addEventListener('pointerdown', function (e) {
 			if (!pointers.has(e.pointerId)) {
@@ -45,48 +49,48 @@ class CometPointerController {
 
 	start(e) {
 		const t = this;
-		if (t.on_start) {
+		if (t.#on_start) {
 			const control_id = `pointer_${e.pointerId}`;
-			const osc_frequency = t.smart_osc_frequency(e.pageX, t.surface.width);
+			const osc_frequency = CometPointerController.smart_osc_frequency(e.pageX, t.surface.width);
 			const osc_velocity = e.pressure;
-			const filter_frequency = t.smart_filter_frequency(e.pageY, t.surface.height);
-			t.on_start(control_id, osc_frequency, osc_velocity, filter_frequency);
+			const filter_frequency = CometPointerController.smart_filter_frequency(e.pageY, t.surface.height);
+			t.#on_start(control_id, osc_frequency, osc_velocity, filter_frequency);
 		}
 	}
 
 	update(e) {
 		const t = this;
-		if (t.on_update) {
+		if (t.#on_update) {
 			const control_id = `pointer_${e.pointerId}`;
-			const osc_frequency = t.smart_osc_frequency(e.pageX, t.surface.width);
+			const osc_frequency = CometPointerController.smart_osc_frequency(e.pageX, t.surface.width);
 			const osc_velocity = e.pressure;
-			const filter_frequency = t.smart_filter_frequency(e.pageY, t.surface.height);
-			t.on_update(control_id, osc_frequency, osc_velocity, filter_frequency);
+			const filter_frequency = CometPointerController.smart_filter_frequency(e.pageY, t.surface.height);
+			t.#on_update(control_id, osc_frequency, osc_velocity, filter_frequency);
 		}
 	}
 
 	stop(e) {
 		const t = this;
-		if (t.on_stop) {
-			t.on_stop(`pointer_${e.pointerId}`);
+		if (t.#on_stop) {
+			t.#on_stop(`pointer_${e.pointerId}`);
 		}
 	}
 
 	/*
-	standard_osc_frequency(mouseX, mouseMaxW) {
+	static standard_osc_frequency(mouseX, mouseMaxW) {
 		const minValue = 27.5;
 		const maxValue = 2000; // 4186.01
 		return ((mouseX / mouseMaxW) * maxValue) + minValue;
 	}
 
-	standard_filter_frequency(mouseY, mouseMaxH) {
+	static standard_filter_frequency(mouseY, mouseMaxH) {
 		const minValue = 27.5;
 		const maxValue = 24000; // Synth.context.sampleRate / 2
 		return ((mouseY / mouseMaxH) * maxValue) + minValue;
 	}
 	*/
 
-	smart_osc_frequency(mouseX, mouseMaxW) {
+	static smart_osc_frequency(mouseX, mouseMaxW) {
 		const minValue = 27.5;
 		const maxValue = 2000; // 4186.01
 		const range = mouseX * 1.0 / mouseMaxW;
@@ -95,7 +99,7 @@ class CometPointerController {
 		return maxValue * multiplier;
 	}
 
-	smart_filter_frequency(mouseY, mouseMaxH) {
+	static smart_filter_frequency(mouseY, mouseMaxH) {
 		const minValue = 27.5;
 		const maxValue = 24000; // Synth.context.sampleRate / 2
 		const range = 1.0 - (mouseY * 1.0 / mouseMaxH);
